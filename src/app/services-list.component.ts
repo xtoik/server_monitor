e33e7c0f9d6b781e3eae212a5f7520997c36354e
xtoik <xtoiko@gmail.com>
2017-11-10 08:45:27 +0100

    First attempt of animating the expansion

--- src/app/services-list.component.ts
+++ src/app/services-list.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 import { Input } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  keyframes
+} from '@angular/animations';
 
 import { Service } from './dtos/service';
 import { ServiceComponent } from './service.component';
@@ -7,7 +15,20 @@ import { ServiceComponent } from './service.component';
 @Component({
   selector: 'services-list',
   templateUrl: './services-list.component.html',
-  styleUrls: ['./services-list.component.css']
+  styleUrls: ['./services-list.component.css'],
+  animations: [
+    trigger('expand', [
+      state('true', style({height: '*'})),
+      transition('true => false', [
+        style({height: '*'}),
+        animate(1000, style({height: 0}))
+      ]),
+      transition('false => true', [
+        style({height: 0}),
+        animate(1000, style({height: '*'}))
+      ]),
+    ])
+  ]
 })
 export class ServicesListComponent {
   @Input() services : Service[]; 

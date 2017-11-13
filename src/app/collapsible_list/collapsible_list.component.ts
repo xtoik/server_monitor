9df3e9ddcc0539e4128e6d085bdf4d31b9470449
xtoik <xtoiko@gmail.com>
2017-11-13 07:00:54 +0100

    Started isolating the collapsible list code in a separate component.

---
+++ src/app/collapsible_list/collapsible_list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'collist',
+    templateUrl: 'collapsible-list.component.html',
+    styleUrls: [ 'collapsible-list.component.css' ]
+})
+export class CollapsibleListComponent {
+
+}
